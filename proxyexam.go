86397d5fe7b63264598e67629bda281cdc95cacf
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-06-15 23:54:41 +0900

    setup scripts

--- proxyexam.go
+++ proxyexam.go
@@ -1,5 +1,9 @@
+/*
+Package proxyexam provides goproxy test/examine suites.
+*/
 package proxyexam
 
+// Version returns current version string.
 func Version() string {
 	return "v0.0.0" // Version
 }
